26a52849c42f1269f5edd768254ef6e6a43a835a
IDGS-801-19002051 <156255910+IDGS-801-19002051@users.noreply.github.com>
2024-01-17 23:29:05 -0600

    SCSDJ 17/01/28

---
+++ arreglos.py
@@ -0,0 +1,48 @@
+class Lista:
+  #declaración de propiedades
+    num1=0
+    lista=[]
+    c=0
+    aux=0
+  #declaración o definición del constructor de la clase
+    def __init__(self,a) -> None:
+        self.num1=a
+        self.lista=[]
+        self.par=[]
+        self.impar=[]
+        self.c=0
+  #declaración de los métodos de la clase
+    def obtener(self):
+        
+        i = self.num1
+        print(i)
+        while i > 0:
+          self.lista.append(int(input("dame el numero {}: ".format(i))))
+          print(i)
+          if self.lista[self.c]%2 == 0:
+            self.par.append(self.lista[self.c])
+          else: 
+             self.impar.append(self.lista[self.c])
+          i -= 1
+          self.c = self.c + 1
+        
+        self.lista.sort()
+        self.par.sort()
+        self.impar.sort()
+        print("lista ordenada: {}".format(self.lista))
+        print("lista con pares: {}".format(self.par))
+        print("lista con impares: {}".format(self.impar))
+        
+        for n in self.lista:
+            if n != self.aux:
+              if self.lista.count(n) > 1:
+                
+                print("el numero {} se repite {} veces".format(n, self.lista.count(n)))
+            self.aux = n; 
+
+def main():
+  obj=Lista(int(input("cuantos números?")))
+  obj.obtener()
+
+if __name__=="__main__":
+  main()
---
+++ asteriscos.py
@@ -0,0 +1,23 @@
+class OperaAst:
+  #declaración de propiedades
+    num1=0
+    ast = ""
+  #declaración o definicion del constructor de la clase
+    def __init__(self,a) -> None:
+        self.num1=a
+        self.ast="*"
+        self.total=""
+  #declaración de los metodos de la clase
+    def construir(self):
+        i = self.num1+1
+        while i > 0:
+          print(self.total)
+          self.total = self.total + self.ast
+          i -= 1
+
+def main():
+  obj=OperaAst(6)
+  obj.construir()
+
+if __name__=="__main__":
+  main()
