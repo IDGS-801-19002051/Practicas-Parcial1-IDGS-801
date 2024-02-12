8b32559296d665dfc09ba9925f65fc0bc5c66823
IDGS-801-19002051 <156255910+IDGS-801-19002051@users.noreply.github.com>
2024-02-12 17:52:58 -0600

    SCSDJ 12/02/2024 Examen

--- forms.py
+++ forms.py
@@ -1,8 +1,17 @@
 from wtforms import Form
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, SelectField, RadioField
 
 class UserForm(Form):
   x1=FloatField("x1")
   x2=FloatField("x2")
   y1=FloatField("y1")
   y2=FloatField("y2")
+  
+class ResisForm(Form):
+  val1 = SelectField('Primera banda', choices=[(0,'Negro'), (1,'Cafe'), (2,'Rojo'),(3,'Naranja'),(4,'Amarillo'),(5,'Verde'),(6,'Azul'),(7,'Violeta'),(8,'Gris'),(9,'Blanco')],default=1,coerce=int)
+  val2 = SelectField('Segunda Banda', choices=[(0,'Negro'), (1,'Cafe'), (2,'Rojo'),(3,'Naranja'),(4,'Amarillo'),(5,'Verde'),(6,'Azul'),(7,'Violeta'),(8,'Gris'),(9,'Blanco')],default=1,coerce=int)
+  val3 = SelectField('Tercera banda', choices=[(1,'Negro'), (10,'Cafe'), (100,'Rojo'),(1000,'Naranja'),(10000,'Amarillo'),(100000,'Verde'),(1000000,'Azul'),(10000000,'Violeta'),(100000000,'Gris'),(1000000000,'Blanco')],default=1,coerce=int)
+  val4 =  RadioField('Tolerancia', choices=[
+        (5,'Oro'), (10,'Plata')],
+        default=1, coerce=int)
+  
--- main.py
+++ main.py
@@ -8,6 +8,31 @@ app=Flask(__name__)
 def operas():
   return render_template("OperaBas.html")
 
+@app.route("/resistencia", methods=["GET","POST"])
+def resistencia(): 
+  resistencia_clase=forms.ResisForm(request.form)
+  if request.method=="POST":
+    val1=resistencia_clase.val1.data
+    val2=resistencia_clase.val2.data
+    val3=resistencia_clase.val3.data
+    val4=resistencia_clase.val4.data
+    colors=["Negro","Cafe","Rojo","Naranja","Amarillo","Verde","Azul","Violeta","Gris","Blanco"]
+    colors2={1:"Negro",10:"Cafe",100:"Rojo",1000:"Naranja",10000:"Amarillo",100000:"Verde",1000000:"Azul",10000000:"Violeta",100000000:"Gris",1000000000:"Blanco"}  
+    colorseng = ["Black","Brown","Red","Orange","yellow","Green","Blue","Violet","Grey","White"]
+    colors2eng={1:"Black",10:"Brown",100:"Red",1000:"Orange",10000:"yellow",100000:"Green",1000000:"Blue",10000000:"Violet",100000000:"Grey",1000000000:"White"}
+    coltol={5:"Gold",10:"Silver"}
+    valt= int(str(val1)+str(val2)) * int(val3)
+    valmin= valt - ((valt*val4)/100)
+    valmax= valt + ((valt*val4)/100)
+    valtem=val4
+    if val4==5:
+      val4= "Dorado "+str(val4)+"%"
+    elif val4==10:
+      val4= "Plateado "+str(val4)+"%"
+    return render_template("resistencia.html", form=resistencia_clase, val1=colors[val1],val2=colors[val2],val4=val4, val3=colors2[val3], valt=valt, valmin=valmin, valmax=valmax, bg1=colorseng[val1],bg2=colorseng[val2],bg3=colors2eng[val3],bg4=coltol[valtem])
+  else:
+    return render_template("resistencia.html", form=resistencia_clase)
+
 @app.route("/distancia", methods=["GET","POST"])
 def distancia():
   distancia_clase=forms.UserForm(request.form)
@@ -18,8 +43,9 @@ def distancia():
     dis4=distancia_clase.y2.data
     dist=math.sqrt(((dis2-dis1)** 2) + ((dis4-dis3)** 2))
     print(dist)
-    
-  return render_template("distancia.html", form=distancia_clase, dist=dist)
+    return render_template("distancia.html", form=distancia_clase, dist=dist)
+  else:
+    return render_template("distancia.html", form=distancia_clase)
 
 
 @app.route("/resultado", methods=["GET","POST"])
